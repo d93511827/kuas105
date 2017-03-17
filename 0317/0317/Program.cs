76075e93b319583d8e44ac0254b50d54c84bf3ca
school <user@1-6>
2017-03-17 11:52:53 +0800

    new

---
+++ 0317/0317/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _0317
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stations = Findstations();
+        }
+        public static List<station> Findstations()
+        {
+            List<station> stations = new List<station>();
+            var xml = XElement.Load(@"C:\Users\user\Desktop\git\0317\data.xml");
+            XNamespace gml = @"http://www.opengis.net/gml/3.2";
+            XNamespace twed = @"http://twed.wra.gov.tw/twedml/opendata";
+            var stationsNode = xml.Descendants(twed + "RiverStageObservatoryProfile").ToList();
+
+            return stations;
+        }
+    }
+}
